d46afc152b6345292e87c01d6163a490d551afc9
todesj <josht2004@gmail.com>
2020-10-23 11:41:52 -0400

    Updated_Meeting_Class

--- lib/src/data/meetings_scheduler/meeting.dart
+++ lib/src/data/meetings_scheduler/meeting.dart
@@ -2,16 +2,27 @@
 import 'package:ramaz/src/data/contact_info.dart';
 
 class Meeting{
-  var meetingDateTime;
+  DateTime dateTime;
   String location;
-  String durration;
+  String duration;
   String studentMessage;
   String teacherMessage;
   ContactInfo student;
   ContactInfo teacher;
   String meetingReason;
-  bool inPerson;
-  Meeting(this.meetingDateTime,this.durration,this.studentMessage, this.teacherMessage, this.student, this.teacher,
-      this.meetingReason,this.inPerson,{this.location});
+  bool isInPerson;
+  bool isApproved;
+  Meeting(
+      this.dateTime,
+      this.duration,
+      this.studentMessage,
+      this.teacherMessage,
+      this.student,
+      this.teacher,
+      this.meetingReason,
+      this.isInPerson,
+      this.isApproved,
+      {this.location}
+      );
 }
 
